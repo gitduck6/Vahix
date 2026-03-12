40793bcfa5c838ebda6ef0c46bf5802daf8f1368
Vahid Khalafov <vahid.khalafovv@gmail.com>
2026-03-12 18:43:00 +0400

    Fixed quotes for \n

--- src/kernel.c
+++ src/kernel.c
@@ -7,7 +7,7 @@ size_t cursor = 0;
 
 void print(const char *msg) {
     for (size_t i = 0; msg[i] != '\0'; ++i) {
-        if (msg[i] == "\n"){
+        if (msg[i] == '\n'){
             cursor += 80 - (cursor % 80);
         }
         VGA_BUFFER[i] = (uint16_t)msg[i] | ((uint16_t)VGA_COLOR << 8);
