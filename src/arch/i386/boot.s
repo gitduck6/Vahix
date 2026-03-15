2104d993bf575168886ff6d1bd8ef2bb0447a681
gitduck6 <warmsot19@gmail.com>
2026-03-15 23:36:20 +0400

    Improved multiboot

--- src/arch/i386/boot.s
+++ src/arch/i386/boot.s
@@ -4,11 +4,17 @@
 .set MAGIC,    0x1BADB002
 .set CHECKSUM, -(MAGIC + FLAGS)
 
-.section .multiboot
+.section .multiboot,"a"
 .align 4
 .long MAGIC
 .long FLAGS
 .long CHECKSUM
+
+.section .bss
+.align 16
+stack_bottom:
+.skip 16384
+stack_top:
 
 .section .text
 .global _start
@@ -16,8 +22,11 @@
 
 _start:
     cli
+    mov $stack_top, %esp
     call kernel_main
 
 hang:
     hlt
     jmp hang
+
+.section .note.GNU-stack,"",@progbits
