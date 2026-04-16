57653b7d9e705be97d76c7641a20bdcfd0e1514f
Vahid Khalafov <vahid.khalafovv@gmail.com>
2026-04-16 23:43:33 +0400

    Fixed random.c

--- src/lib/random.c
+++ src/lib/random.c
@@ -1,6 +1,7 @@
 #include <lib/random.h>
 
-int random(char * seed){
-    (*seed) = ((*seed * 1664525) + 1013904223);
-    return *seed;
+int random(void){
+    static int random_number = 1; // I need a seed to put here
+    random_number = (random_number * 1103515245 + 12345) & 0x7fffffff;
+    return random_number;
 }
